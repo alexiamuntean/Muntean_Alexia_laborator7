edb973c2f77d3eff3f823d9de5f7be9024b93ce4
alexia2929 <132937530+alexia2929@users.noreply.github.com>
2023-11-29 18:49:11 +0200

    Laborator8

---
+++ ListPage.xaml.cs
@@ -0,0 +1,24 @@
+using Muntean_Alexia_laborator7.Models;
+namespace Muntean_Alexia_laborator7;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+}
