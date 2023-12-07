73bd83300f2073e20ef78005b43fc18f74325bb4
alexia2929 <132937530+alexia2929@users.noreply.github.com>
2023-12-07 21:14:07 +0200

    Laborator9

---
+++ ValidationBehavior.cs
@@ -0,0 +1,22 @@
+﻿using Muntean_Alexia_laborator7.Models;
+namespace Muntean_Alexia_laborator7
+{
+    class ValidationBehavior : Behavior <Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor = string.IsNullOrEmpty(args.NewTextValue) ? Color.FromRgba("#AA4A44") :Color.FromRgba("#FFFFFF");
+        }
+    }
+
+}
